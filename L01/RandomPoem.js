037eb3315ddccc3b640e3fad6215b67f83c0d3fc
Gats237 <henning.silas.pils@hs-furtwangen.de>
2022-03-24 21:01:31 +0100

    Funktion erneut hochgeladen

--- L01/RandomPoem.js
+++ L01/RandomPoem.js
@@ -1,14 +1,24 @@
 "use strict";
 var RandomPoem;
 (function (RandomPoem) {
+    // Arrays with subjects, verb und object
     let subject = ["Rick", "Morty", "Summer", "Beth", "Jerry", "Meeseeks", "Poopy Butthole", "Birdperson"];
     let verb = ["eats", "curses", "laughs at", "watches", "travels to", "destroys", "loves", "flies to"];
     let object = ["Spagetti with Tomatosauce", "the equinox", "Butter", "another universe", "cream of Tartar", "Highway to hell", "Marvel", "Gravel"];
     console.log(subject);
     console.log(verb);
     console.log(object);
+    // For-Schleife, die rückwärts durch das Array zählt
     for (let i = subject.length; i >= 1; i--) {
+        let randomPoem;
         console.log(i);
+    }
+    ;
+    function getVerse(_subject, _verb, _object) {
+        let subjectEmpty = [];
+        let verbEmpty = [];
+        let objectEmpty = [];
+        return;
     }
     ;
 })(RandomPoem || (RandomPoem = {}));
