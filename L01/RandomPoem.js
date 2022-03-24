a999a9ea33da8c06a4f784eefb3e221e6a447d55
Gats237 <henning.silas.pils@hs-furtwangen.de>
2022-03-24 20:19:04 +0100

    Konsole

--- L01/RandomPoem.js
+++ L01/RandomPoem.js
@@ -1,7 +1,13 @@
 "use strict";
-var randomPoem;
-(function (randomPoem) {
-    console.log("Hello World");
-    v;
-})(randomPoem || (randomPoem = {}));
+var RandomPoem;
+(function (RandomPoem) {
+    let subject = ["Rick", "Morty", "Summer", "Beth", "Jerry", "Meeseeks", "Poopy Butthole", "Birdperson"];
+    let verb = ["eats", "curses", "laughs at", "watches", "travels to", "destroys", "loves", "flies to"];
+    let object = ["Spagetti with Tomatosauce", "the equinox", "Butter", "another universe", "cream of Tartar", "Highway to hell", "Marvel", "Gravel"];
+    console.log(subject);
+    console.log(verb);
+    console.log(object);
+    for (; ; )
+        ;
+})(RandomPoem || (RandomPoem = {}));
 //# sourceMappingURL=RandomPoem.js.map
