817dd06e85716c66cab9e9a1f2d51ee4c3688485
Gats237 <henning.silas.pils@hs-furtwangen.de>
2022-03-24 20:18:41 +0100

    Arrays erstellt

--- L01/RandomPoem.ts
+++ L01/RandomPoem.ts
@@ -1,8 +1,12 @@
-namespace randomPoem {
-    console.log("Hello World");
-v
+namespace RandomPoem {
 
+    let subject: string[] = ["Rick", "Morty", "Summer", "Beth", "Jerry", "Meeseeks", "Poopy Butthole", "Birdperson"];
+    let verb: string[] = ["eats", "curses", "laughs at", "watches", "travels to", "destroys", "loves", "flies to"];
+    let object: string[] = ["Spagetti with Tomatosauce", "the equinox", "Butter", "another universe", "cream of Tartar", "Highway to hell", "Marvel", "Gravel"]; 
 
+    console.log(subject); console.log(verb); console.log(object);
+
+    for; 
 
 
 }
