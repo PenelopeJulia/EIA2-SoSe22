3c3935b260828756183831318d59b15654f0e679
Gats237 <henning.silas.pils@hs-furtwangen.de>
2022-03-24 21:00:42 +0100

    For-Schleife und Funktion erstellt

--- L01/RandomPoem.ts
+++ L01/RandomPoem.ts
@@ -1,13 +1,27 @@
 namespace RandomPoem {
 
+    // Arrays with subjects, verb und object
     let subject: string[] = ["Rick", "Morty", "Summer", "Beth", "Jerry", "Meeseeks", "Poopy Butthole", "Birdperson"];
     let verb: string[] = ["eats", "curses", "laughs at", "watches", "travels to", "destroys", "loves", "flies to"];
     let object: string[] = ["Spagetti with Tomatosauce", "the equinox", "Butter", "another universe", "cream of Tartar", "Highway to hell", "Marvel", "Gravel"]; 
 
     console.log(subject); console.log(verb); console.log(object);
 
+    // For-Schleife, die rückwärts durch das Array zählt
     for (let i: number = subject.length; i >= 1; i--){
+
+        let randomPoem
         console.log(i);
+       
+      
+    };
+
+    function getVerse(_subject: string, _verb: string, _object: string): void {
+        let subjectEmpty: string[] = [];
+        let verbEmpty: string[] = [];
+        let objectEmpty: string[] = [];
+
+        return 
     };
 
 
