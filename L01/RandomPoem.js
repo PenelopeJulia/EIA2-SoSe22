7ced1f6a1381c8e1bad940fd853cac5b11f98416
Gats237 <henning.silas.pils@hs-furtwangen.de>
2022-03-24 21:18:51 +0100

    Arrays randomizen

--- L01/RandomPoem.js
+++ L01/RandomPoem.js
@@ -8,8 +8,9 @@ var RandomPoem;
     console.log(subject);
     console.log(verb);
     console.log(object);
-    // For-Schleife, die rückwärts durch das Array zählt
+    // For-Schleife,that counts backwards through the arrays
     for (let i = subject.length; i >= 1; i--) {
+        // Declare Variable that contains value of randomized Words
         let randomPoem;
         console.log(i);
     }
@@ -18,6 +19,10 @@ var RandomPoem;
         let subjectEmpty = [];
         let verbEmpty = [];
         let objectEmpty = [];
+        // Randomize Words in Array. Source: https://timonweb.com/javascript/how-to-get-a-random-value-from-a-javascript-array/#:~:text=Math.floor%20%28Math.random%20%28%29%20%2A%20colors.length%29%2C%20gives%20us%20a,index%20larger%20than%20the%20length%20of%20the%20array.
+        let subjectRandom = Math.floor(Math.random() * subject.length);
+        let verbRandom = Math.floor(Math.random() * verb.length);
+        let objectRandom = Math.floor(Math.random() * object.length);
         return;
     }
     ;
--- L01/RandomPoem.ts
+++ L01/RandomPoem.ts
@@ -7,9 +7,10 @@ namespace RandomPoem {
 
     console.log(subject); console.log(verb); console.log(object);
 
-    // For-Schleife, die rückwärts durch das Array zählt
+    // For-Schleife,that counts backwards through the arrays
     for (let i: number = subject.length; i >= 1; i--){
 
+        // Declare Variable that contains value of randomized Words
         let randomPoem
         console.log(i);
        
@@ -21,6 +22,11 @@ namespace RandomPoem {
         let verbEmpty: string[] = [];
         let objectEmpty: string[] = [];
 
+        // Randomize Words in Array. Source: https://timonweb.com/javascript/how-to-get-a-random-value-from-a-javascript-array/#:~:text=Math.floor%20%28Math.random%20%28%29%20%2A%20colors.length%29%2C%20gives%20us%20a,index%20larger%20than%20the%20length%20of%20the%20array.
+        let  subjectRandom = Math.floor(Math.random() * subject.length);
+        let  verbRandom = Math.floor(Math.random() * verb.length);
+        let  objectRandom = Math.floor(Math.random() * object.length);
+    
         return 
     };
 
